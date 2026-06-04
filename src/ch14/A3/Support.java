98923dad4323b184ce99cc53a6956ce6d7904f5e
daniswings <dain@duksung.ac.kr>
2026-06-04 10:37:12 +0900

    6월 4일 수업 전 커밋

--- src/ch14/A3/Support.java
+++ src/ch14/A3/Support.java
@@ -17,13 +17,15 @@ public abstract class Support {
 
     // 트러블 해결 절차를 결정한다 
     public void support(Trouble trouble) {
+        // 핵심: 루프 한 번 돌 때마다 obj.next로 obj가 바뀜
+        // 재귀적으로 호출되던 게 for 루프로도 가능
         for (Support obj = this; true; obj = obj.next) {
             if (obj.resolve(trouble)) {
                 obj.done(trouble);
-                break;
-            } else if (obj.next == null) {
-                obj.fail(trouble);
-                break;
+                break; // 해결이 되면 루프를 빠져나간다
+            } else if (obj.next == null) { // 뒷사람(해결자)가 없으면
+                obj.fail(trouble); // 실패
+                break; // 실패했어도 루프를 빠져나감
             }
         }
     }
